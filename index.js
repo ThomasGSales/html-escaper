76f2efb47f18177d43a2dc886f4ef237beb00135
thomasgsales <tgaabriel444@gmail.com>
2024-12-28 15:41:22 -0300

    funcao que pergunta o caminho ao usuario

--- index.js
+++ index.js
@@ -29,3 +29,14 @@ function escapeHtmlFile(inputFilePath, outputFilePath){
         process.exit(1)
     }
 }
+
+function askFilePath(question){
+    const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+
+    return new Promise((resolve)=>{
+        rl.question(question, (answer)=>{
+            resolve(answer)
+            rl.close()
+        })
+    })
+}
